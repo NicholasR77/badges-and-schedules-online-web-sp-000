ad71b0106ee7aebde5831505a21b3f5f7fd624bc
Nicholas Robbins <nl7robbins@icloud.com>
2020-04-21 17:03:42 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,8 +1,5 @@
 # Write your code here.
 
-names = ["John", "Deshawn", "Bill", "Jennie"]
-speakers = ["Ash", "Misty", "Brock"]
-
 def badge_maker(name)
   return "Hello, my name is #{name}."
 end
@@ -15,9 +12,7 @@ def assign_rooms(speakers)
   speakers.collect.with_index {|speaker, position|"Hello, #{speaker}! You'll be assigned to room #{position + 1}!"}
 end
 
-def printer(names, speakers)
-  puts batch_badge_creator(names)
-  puts assign_rooms(speakers)
+def printer(people)
+  puts batch_badge_creator(people)
+  puts assign_rooms(people)
 end
-
-printer(names, speakers)
